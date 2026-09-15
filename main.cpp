e6d7a46f8264b3d2ac38dde9f7c9f125586dae30
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:04:34 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "FACET_Forensic_Correlation_and_Evidence_Intelligence_Network" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
